a6bbdd81386d27ae98d02717dc935211fb0cb33d
sahaya.thanislas <georgetsahaya@gmail.com>
2014-03-02 13:15:22 +1100

    Code refactoring

---
+++ src/com/example/mynewsreader/data/NRDict.java
@@ -0,0 +1,15 @@
+package com.example.mynewsreader.data;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class NRDict implements Serializable {
+	
+	public Integer identifier ;
+	public ArrayList<NRNewsItem> items = new ArrayList<NRNewsItem>();
+	public String name ;
+
+}
---
+++ src/com/example/mynewsreader/data/NRNewsItem.java
@@ -0,0 +1,16 @@
+package com.example.mynewsreader.data;
+
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class NRNewsItem implements Serializable {
+	public String dateLine ;
+	public String headLine ;
+	public Integer identifier ;
+	public String slugLine ;
+	public String thumbnailImageHref ;
+	public String tinyUrl ;
+	public String type ;
+	public String webHref ;
+}
